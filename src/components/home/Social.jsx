847e04d52836c8e9ea7f138029a075b80080fa49
Libawo <libawod@gmail.com>
2023-01-08 05:03:10 +0100

    react js

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.instagram.com/accounts/login/" className="home__social-icon" >
+            <i className='uil uil-instagram'></i>
+        </a>
+        <a href="https://dribbble.com/Dieu100" className="home__social-icon" >
+        <i className='uil uil-dribbble'></i>
+
+        </a>
+        <a href="https://gitlab.com/libawod" className="home__social-icon" >
+        <i className='uil uil-github-alt'></i>
+
+        </a>
+    </div>
+  )
+}
+
+export default Social
